1f8d58922a7e6002c8859dd0ae561ec12e855a9d
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2014-12-12 15:44:02 +0100

    show_workflow now writes the graph before showing

--- gilles_workflows/utils.py
+++ gilles_workflows/utils.py
@@ -1,4 +1,5 @@
 def show_workflow(workflow):
     from IPython.display import Image
     import os
+    workflow.write_graph()
     return Image(os.path.join(workflow.base_dir, workflow.name, 'graph.dot.png'))
