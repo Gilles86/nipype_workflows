4d0ee60f53c40d43e2ba7061e7885589dc0a9a00
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2015-11-12 13:59:42 +0100

    Easy import of bfsl workflow

--- gilles_workflows/__init__.py
+++ gilles_workflows/__init__.py
@@ -2,6 +2,6 @@ from registration import create_fsl_afni_registration_workflow
 from extract_rois import create_extract_mni_roi_workflow
 from utils import show_workflow
 from interfaces import FDR
-from model import create_fdr_threshold_workflow
+from model import create_fdr_threshold_workflow, create_modelfit_workflow_bfsl
 
 #from . import extract_rois
