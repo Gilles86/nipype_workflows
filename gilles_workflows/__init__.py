82cf6ff814d4ab1f04fa485f38982d3c728238e8
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2015-11-24 14:00:55 +0100

    ANTS not AFNI

--- gilles_workflows/__init__.py
+++ gilles_workflows/__init__.py
@@ -1,4 +1,4 @@
-from registration import create_fsl_afni_registration_workflow
+from registration import create_fsl_ants_registration_workflow
 from extract_rois import create_extract_mni_roi_workflow
 from utils import show_workflow
 from interfaces import FDR
